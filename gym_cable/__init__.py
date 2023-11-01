297c7dbf10af39f8ddfa3a35d0074d3473c0f807
y_yamaguchi <chrisrerei125815@gmail.com>
2023-11-01 17:34:28 +0900

    gymnasium-roboticsからベースとなるコードをコピペ

---
+++ gym_cable/__init__.py
@@ -0,0 +1,11 @@
+from gymnasium.envs.registration import register
+
+
+def register_robotics_envs():
+    """Register all environment ID's to Gymnasium."""
+    
+    register(
+        id="MZ04CableGrasp-v0",
+        entry_point="gym_cable.envs.mz04.cable_grasp:MujocoMZ04CableGraspEnv",
+        max_episode_steps=100,
+    )
