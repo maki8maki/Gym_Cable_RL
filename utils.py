059da706c91458ce69a08ef9a13951c94654eb07
y_yamaguchi <chrisrerei125815@gmail.com>
2023-11-14 07:34:49 +0900

    アニメーション関数の追加

---
+++ utils.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def anim(frames):
+    plt.figure(figsize=(frames[0].shape[1]/72.0, frames[0].shape[0]/72.0), dpi=72)
+    patch = plt.imshow(frames[0])
+    plt.axis('off')
+
+    def animate(i):
+        patch.set_data(frames[i])
+
+    anim = animation.FuncAnimation(plt.gcf(), animate, frames=len(frames), interval=1000)
+    plt.show()
