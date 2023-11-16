39fd2523439ccb2c5c622fef3ed9038e9d3f92e2
maki8maki <maki8maki.tech@gmail.com>
2023-11-16 12:30:06 +0900

    seed固定関数の追加とアニメーションのインターバルの変更

--- utils.py
+++ utils.py
@@ -1,5 +1,18 @@
+import random
+import numpy as np
+import torch
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
 
 def anim(frames):
     plt.figure(figsize=(frames[0].shape[1]/72.0/4, frames[0].shape[0]/72.0/4), dpi=72)
@@ -9,5 +22,5 @@ def anim(frames):
     def animate(i):
         patch.set_data(frames[i])
 
-    anim = animation.FuncAnimation(plt.gcf(), animate, frames=len(frames), interval=1000)
+    anim = animation.FuncAnimation(plt.gcf(), animate, frames=len(frames), interval=50)
     plt.show()
