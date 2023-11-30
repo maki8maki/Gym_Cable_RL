505795b5a9c529aa4ca347155b61005007da6c01
y_yamaguchi <chrisrerei125815@gmail.com>
2023-11-30 21:58:22 +0900

    コードの整理と微修正

--- agents/utils.py
+++ agents/utils.py
@@ -1,5 +1,6 @@
 from collections import deque
 import numpy as np
+import torch
 import torch.nn as nn
 
 def size_after_conv(h, ksize, stride=1, padding=0):
@@ -40,3 +41,43 @@ class ReplayBuffer:
         successes   = np.array([self.memory[index]['success'] for index in batch_indexes])
         dones       = np.array([self.memory[index]['done'] for index in batch_indexes])
         return {'states': states, 'next_states': next_states, 'rewards': rewards, 'actions': actions, 'successes': successes, 'dones': dones}
+
+class RL():
+    def __init__(self):
+        pass
+    
+    def batch_to_tensor(self, batch, key_list=['states', 'actions', 'next_states', 'rewards', 'dones']):
+        return_list = []
+        for key in key_list:
+            if isinstance(batch[key], torch.Tensor):
+                item = batch[key]
+                if item.dtype != torch.float:
+                    item = item.to(torch.float)
+                if item.device != self.device:
+                    item = item.to(self.device)
+            else:
+                item = torch.tensor(batch[key], dtype=torch.float, device=self.device)
+            return_list.append(item)
+        return return_list
+    
+    def update_from_batch(self, batch):
+        raise NotImplementedError
+    
+    def get_action(self, state, deterministic=False):
+        raise NotImplementedError
+    
+    def save(self, path):
+        torch.save(self.state_dict(), path)
+    
+    def load(self, path):
+        state_dict = torch.load(path, map_location=self.device)
+        self.load_state_dict(state_dict)
+    
+    def state_dict(self):
+        raise NotImplementedError()
+    
+    def load_state_dict(self, state_dict):
+        raise NotImplementedError()
+    
+    def eval(self):
+        raise NotImplementedError()
