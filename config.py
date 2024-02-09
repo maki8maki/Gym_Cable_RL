7136841be29192dd0a062bb220cf854814076c7b
y_yamaguchi <chrisrerei125815@gmail.com>
2024-02-09 22:05:20 +0900

    モデルもyamlファイルで変更できようにした

--- config.py
+++ config.py
@@ -1,140 +1,103 @@
 import dataclasses
-from typing import Dict, Tuple
 import torch
 import torch.nn as nn
-import numpy as np
-from gymnasium import spaces
+from absl import logging
+from copy import deepcopy
 import hydra
+from omegaconf import OmegaConf
+import dacite
 
 from agents import utils
+from agents.DCAE import DCAE
+from agents.SAC import SAC
+from utils import set_seed
 
 @dataclasses.dataclass
 class FEConfig:
-    model_name: str
-    lr: float = 1e-3
     img_width: int = 108
     img_height: int = 72
-    img_dim : dataclasses.InitVar[int] = 4
-    image_size: Tuple[int, int, int] = dataclasses.field(init=False)
+    img_channel : int = 4
     hidden_dim: int = 20
+    model_name: str = ''
+    _model: dataclasses.InitVar[dict] = None
+    model: nn.Module = dataclasses.field(default=None)
     _trans: dataclasses.InitVar[dict] = None
-    trans: nn.Module = dataclasses.field(init=False)
-    _net_activation: dataclasses.InitVar[dict] = None
-    net_activation: nn.Module = dataclasses.field(init=False)
-    _loss_func: dataclasses.InitVar[dict] = None
-    loss_func: nn.Module = dataclasses.field(init=False)
+    trans: nn.Module = dataclasses.field(default=None, repr=False)
     
-    def __post_init__(self, img_dim, _trans, _net_activation, _loss_func):
-        self.image_size = (self.img_height, self.img_width, img_dim)
-        if _trans is not None:
-            self.trans = hydra.utils.instantiate(_trans, img_width=self.img_width, img_height=self.img_height)
-        else:
+    def __post_init__(self, _model, _trans):
+        if _model is None:
+            self.model = DCAE(img_height=self.img_height, img_width=self.img_width, img_channel=self.img_channel, hidden_dim=self.hidden_dim)
+        if _trans is None:
             self.trans = utils.MyTrans(img_width=self.img_width, img_height=self.img_height)
-        if _net_activation is not None:
-            self.net_activation = hydra.utils.instantiate(_net_activation)
-        else:
-            self.net_activation = nn.GELU()
-        if _loss_func is not None:
-            self.loss_func = hydra.utils.instantiate(_loss_func, channel=img_dim)
-        else:
-            self.loss_func = utils.SSIMLoss(channel=img_dim)
-    
-    def get_cfg_dict(self):
-        # 辞書を作成し、不要なデータの削除を行う
-        cfg_dict = dataclasses.asdict(self)
-        del_list = ['img_width', 'img_height', 'trans', 'model_name']
-        for key in del_list:
-            del cfg_dict[key] 
-        return cfg_dict
+
+    def convert(self, _cfg: OmegaConf):
+        self_copy = deepcopy(self)
+        if _cfg._model:
+            self_copy.model = hydra.utils.instantiate(_cfg._model)
+        if _cfg._trans:
+            self_copy.trans = hydra.utils.instantiate(_cfg._trans)
+        return self_copy
 
 @dataclasses.dataclass
 class RLConfig:
-    gamma: float = 0.7
-    batch_size: int = 50
-    lr: float = 1e-3
     obs_dim: int = 6
     act_dim: int = 6
+    _model: dataclasses.InitVar[dict] = None
+    model: utils.RL = dataclasses.field(default=None)
     
-    def get_cfg_dict(self):
-        # 辞書を作成し、不要なデータの削除を行う
-        cfg_dict = dataclasses.asdict(self)
-        del_list = ['obs_dim', 'act_dim']
-        for key in del_list:
-            del cfg_dict[key]
-        return cfg_dict
+    def __post_init__(self, _model):
+        if _model is None:
+            self.model = SAC(obs_dim=self.obs_dim, act_dim=self.act_dim)
+
+    def convert(self, _cfg: OmegaConf):
+        self_copy = deepcopy(self)
+        if _cfg._model:
+            self_copy.model = hydra.utils.instantiate(_cfg._model)
+        return self_copy
 
 @dataclasses.dataclass
 class Config:
     fe: FEConfig
     rl: RLConfig
     basename: str
-    nsteps: int = 100
+    nsteps: int = dataclasses.field(default=100, repr=False)
     memory_size: int = 10000
     nepisodes: int = 5000
-    nevalepisodes: int = 5
+    nevalepisodes: int = dataclasses.field(default=5, repr=False)
     update_every: int = 50
-    save_anim_num: int = 10
-    eval_num: int = 100
+    save_anim_num: int = dataclasses.field(default=10, repr=False)
+    eval_num: int = dataclasses.field(default=100, repr=False)
     device: str = 'cpu'
-    _replay_buffer: dataclasses.InitVar[dict] = None
-    replay_buffer: utils.Buffer = dataclasses.field(init=False)
+    seed: dataclasses.InitVar[int] = None
+    replay_buffer: utils.Buffer = dataclasses.field(default=utils.ReplayBuffer(memory_size=memory_size), repr=False)
     gathering_data: bool = False
-    data_name: str = dataclasses.field(init=False)
-    output_dir: str = dataclasses.field(init=False)
+    buffer_name: str = dataclasses.field(init=False)
+    output_dir: str = './logs'
     
-    def __post_init__(self, _replay_buffer):
+    def __post_init__(self, seed):
+        if seed is not None:
+            set_seed(seed)
+        if self.device == 'cpu':
+            logging.warning('You are using CPU!!')
         if self.device=='cuda' and not torch.cuda.is_available():
             self.device = 'cpu'
-        if _replay_buffer is not None:
-            self.replay_buffer = hydra.utils.instantiate(_replay_buffer, memory_size=self.memory_size)
-        else:
-            self.replay_buffer = utils.ReplayBuffer(memory_size=self.memory_size)
-        self.basename += f'_o-{self.rl.obs_dim}_a-{self.rl.act_dim}'
-        self.data_name = f'buffer_o-{self.rl.obs_dim}_a-{self.rl.act_dim}_w-hs_{self.memory_size}.pcl'
+            logging.warning('Device changed to CPU!!')
+        if self.fe.model is not None:
+            self.fe.model.to(self.device)
+        if self.rl.model is not None:
+            self.rl.model.to(self.device)
+        self.buffer_name = f'buffer_o-{self.rl.obs_dim}_a-{self.rl.act_dim}_w-hs_{self.memory_size}.pcl'
         self.output_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
-    
-    def get_fe_cfg_dict(self):
-        cfg_dict = self.fe.get_cfg_dict() 
-        return cfg_dict
-    
-    def get_rl_cfg_dict(self, obs_space: spaces.Box=None, act_space: spaces.Box=None):
-        cfg_dict = self.rl.get_cfg_dict()
 
-        hidden_low = np.full(self.fe.hidden_dim, -1.0)
-        hidden_high = np.full(self.fe.hidden_dim, 1.0)
-        if obs_space is None:
-            obs_space_low = np.concatenate([hidden_low, np.full(self.rl.obs_dim, -1.0)])
-            obs_space_high = np.concatenate([hidden_low, np.full(self.rl.obs_dim, 1.0)])
-        else:
-            obs_space_low = np.concatenate([hidden_low, obs_space.low[:self.rl.obs_dim]])
-            obs_space_high = np.concatenate([hidden_high, obs_space.high[:self.rl.obs_dim]])
-        observation_space = spaces.Box(low=obs_space_low, high=obs_space_high, dtype=np.float64)
-        if act_space is None:
-            action_space = spaces.Box(low=-1.0, high=1.0, shape=(self.rl.act_dim,), dtype=np.float64)
-        else:
-            action_space = spaces.Box(low=act_space.low[:self.rl.act_dim], high=act_space.high[:self.rl.act_dim], dtype=np.float64)
-        
-        # データの追加
-        cfg_dict['observation_space'] = observation_space
-        cfg_dict['action_space'] = action_space
-
-        return cfg_dict
-    
-    def get_cfg_dict(self, fe_config: Dict[str, any]=None, rl_config: Dict[str, any]=None):
-        cfg_dict = dataclasses.asdict(self)
-        del_list = ['fe', 'rl', 'device', 'replay_buffer']
-        for key in del_list:
-            del cfg_dict[key]
-        if fe_config is None:
-            fe_config = self.fe.get_cfg_dict()
-        for key, val in fe_config.items():
-            if not isinstance(val, (int, float, str, bool, torch.Tensor)):
-                continue
-            cfg_dict['fe_'+key] = val
-        if rl_config is None:
-            rl_config = self.rl.get_cfg_dict()
-        for key, val in rl_config.items():
-            if not isinstance(val, (int, float, str, bool, torch.Tensor)):
-                continue
-            cfg_dict['rl_'+key] = val
-        return cfg_dict
+    @classmethod
+    def convert(cls, _cfg: OmegaConf):
+        cfg = dacite.from_dict(data_class=cls, data=OmegaConf.to_container(_cfg))
+        cfg.fe = cfg.fe.convert(OmegaConf.create(_cfg.fe))
+        if _cfg.rl._model:
+            _cfg.rl._model.obs_dim = cfg.rl.obs_dim + cfg.fe.hidden_dim
+        cfg.rl = cfg.rl.convert(OmegaConf.create(_cfg.rl))
+        cfg.fe.model.to(cfg.device)
+        cfg.rl.model.to(cfg.device)
+        cfg.basename = _cfg.basename
+        cfg.replay_buffer = hydra.utils.instantiate(_cfg._replay_buffer)
+        return cfg
--- rl_w-ae_cl2.py
+++ rl_w-ae_cl2.py
@@ -1,64 +1,46 @@
-import torch
-from absl import logging
+import numpy as np
 import os
 import hydra
 from omegaconf import OmegaConf
-import dacite
-import gymnasium as gym
-import gym_cable
 
-from utils import set_seed, anim
-from agents.SAC import SAC
-from agents.DCAE import DCAE
+from utils import anim, yes_no_input
 from config import Config
-from executer import CLExecuter
+from executer import CLCombExecuter
 
 @hydra.main(config_path='conf/', config_name='config', version_base=None)
 def main(_cfg: OmegaConf):
-    cfg = dacite.from_dict(data_class=Config, data=OmegaConf.to_container(_cfg))
-    print(cfg)
-    
-    seed = 42
-    set_seed(seed)
-    
-    if cfg.device=='cpu':
-        logging.warning('You are using CPU!!')
-    
-    gym_cable.register_robotics_envs()
-    env = gym.make("MZ04CableGrasp-v0", render_mode="rgb_array", max_episode_steps=cfg.nsteps, is_random=False)
-
-    fe_config = cfg.get_fe_cfg_dict()
-    model = DCAE(**fe_config).to(cfg.device)
-    model.load_state_dict(torch.load(f'./model/{cfg.fe.model_name}', map_location=cfg.device))
-    model.eval()
-    
-    rl_config = cfg.get_rl_cfg_dict(env.observation_space['observation'], env.action_space)
-    agent = SAC(**rl_config)
+    cfg = Config.convert(_cfg)
+    print(f'\n{cfg}\n')
     
     cl_scheduler = []
     options = {'diff_ratio': 0.1}
-    eps = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]
+    eps = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]) * cfg.nepisodes
     for ep in eps:
-        cl_scheduler.append([ep, options])
-        options['diff_ratio'] += 0.1
+        cl_scheduler.append([ep, options.copy()])
+        options['diff_ratio'] = min(options['diff_ratio']+0.1, 1.0)
+    print(cl_scheduler)
     
-    cl_executer = CLExecuter(env=env, cfg=cfg, fe=model, rl=agent, cl_scheduler=cl_scheduler)
+    if not yes_no_input('fe.model_name and basename'):
+        exit()
+    
+    cl_executer = CLCombExecuter(env_name='MZ04CableGrasp-v0', cfg=cfg, cl_scheduler=cl_scheduler)
+    del cfg, _cfg
 
-    cl_executer.gathering_data(rl_config['action_space'])
+    cl_executer.gathering_data()
 
     # train and eval
     frames = []
     titles = []
     cl_executer.train_epsiode_loop(frames=frames, titles=titles)
-    anim(frames, titles=titles, filename=f'{cfg.output_dir}/{cfg.basename}-1.mp4', show=False)
+    anim(frames, titles=titles, filename=f'{cl_executer.cfg.output_dir}/{cl_executer.cfg.basename}-1.mp4', show=False)
 
     # test
     frames = []
     titles= []
     cl_executer.test_step_loop(frames=frames, titles=titles)
-    anim(frames, titles=titles, filename=f'{cfg.output_dir}/{cfg.basename}-2.mp4', show=False)
-    cl_executer.rl.save(os.path.join(os.getcwd(), 'model', f'{cfg.basename}.pth'))
-    cl_executer.rl.save(os.path.join(cfg.output_dir, f'{cfg.basename}.pth'))
+    anim(frames, titles=titles, filename=f'{cl_executer.cfg.output_dir}/{cl_executer.cfg.basename}-2.mp4', show=False)
+    cl_executer.cfg.rl.model.save(os.path.join(os.getcwd(), 'model', f'{cl_executer.cfg.basename}.pth'))
+    cl_executer.cfg.rl.model.save(os.path.join(cl_executer.cfg.output_dir, f'{cl_executer.cfg.basename}.pth'))
 
     cl_executer.close()
 
