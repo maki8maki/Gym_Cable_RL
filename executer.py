ef1a111fde32285903ff49232d2e58037bfb52f1
yuki_y <chrisrerei125815@gmail.com>
2024-03-01 15:53:21 +0900

    train_feをconfigを使う形式に変更

--- executer.py
+++ executer.py
@@ -1,34 +1,25 @@
 from tqdm import tqdm
+from typing import Union
 import numpy as np
+import random
 import sys
 import dill
 import os
-import torch
+import torch as th
 from torch.utils.tensorboard import SummaryWriter
 import gymnasium as gym
 import gym_cable
 
-from utils import return_transition, check_freq, anim
-from config import CombConfig, PPOConfig
+from utils import return_transition, check_freq, anim, EarlyStopping
+from config import CombConfig, PPOConfig, TrainFEConfig
 from agents.buffer import PrioritizedReplayBuffer
 
-class CombExecuter:
-    def __init__(self, env_name: str, cfg: CombConfig, options=None):
-        gym_cable.register_robotics_envs()
-        self.env = gym.make(env_name, render_mode="rgb_array", max_episode_steps=cfg.nsteps, is_random=False)
-        self.writer = SummaryWriter(log_dir=cfg.output_dir)
-        self.act_space = gym.spaces.Box(
-            low = self.env.action_space.low[:cfg.rl.act_dim],
-            high = self.env.action_space.high[:cfg.rl.act_dim]
-        )
-        self.cfg = cfg
-        self.options = options
-        self.update_count = 1
-        
-        self.cfg.fe.model.load_state_dict(torch.load(f'./model/{self.cfg.fe.model_name}', map_location=self.cfg.device))
-        self.cfg.fe.model.eval()
-        self.cfg.rl.model.train()
-    
+class Executer:
+    env: gym.Env
+    cfg: Union[CombConfig, PPOConfig, TrainFEConfig]
+    options: dict = None
+    writer: SummaryWriter
+
     def normalize_state(self, state):
         # 連続値の状態を[-1,1]の範囲に正規化
         normalized_state = {}
@@ -40,17 +31,135 @@ class CombExecuter:
     
     def obs2state(self, obs, image_list=['rgb_image', 'depth_image']):
         normalized_obs = self.normalize_state(obs)
-        image = normalized_obs[image_list[0]]
-        for name in image_list[1:]:
-            image = np.concatenate([image, normalized_obs[name]], axis=2)
-        image = torch.tensor(self.cfg.fe.trans(image), dtype=torch.float, device=self.cfg.device)
-        hs = self.cfg.fe.model.forward(image).cpu().squeeze().detach().numpy()
-        state = np.concatenate([hs, normalized_obs['observation'][:self.cfg.rl.obs_dim]])
-        return state
+        if len(image_list) > 0:
+            image = normalized_obs[image_list[0]]
+            for name in image_list[1:]:
+                image = np.concatenate([image, normalized_obs[name]], axis=2)
+            state = {'observation': normalized_obs['observation'], 'image': self.cfg.fe.trans(image)}
+            return state
+        else:
+            return normalized_obs['observation']
     
     def reset_get_state(self):
         obs, _ = self.env.reset(options=self.options)
         state = self.obs2state(obs)
+        return state
+    
+    def close(self):
+        self.env.close()
+        self.writer.flush()
+        self.writer.close()
+
+class FEExecuter(Executer):
+    def __init__(self, env_name: str, cfg: TrainFEConfig):
+        super().__init__()
+        gym_cable.register_robotics_envs()
+        self.env = gym.make(env_name, render_mode="rgb_array", max_episode_steps=cfg.nsteps, is_random=cfg.is_random)
+        self.writer = SummaryWriter(log_dir=cfg.output_dir)
+        self.cfg = cfg
+        model_path1 = os.path.join(os.getcwd(), 'model', cfg.fe.model_name)
+        model_path2 = os.path.join(cfg.output_dir, cfg.fe.model_name)
+        self.es = EarlyStopping(patience=self.cfg.es_patience, paths=[model_path1, model_path2], trace_func=tqdm.write)
+        self.cfg.fe.model.train()
+        self.imgs = None
+    
+    def gathering_data(self):
+        data_path = os.path.join(os.getcwd(), 'data', self.cfg.data_name)
+        if self.cfg.gathering_data:
+            imgs = []
+            state = self.reset_get_state()
+            if self.cfg.with_init:
+                for _ in tqdm(range(self.cfg.data_size)):
+                    imgs.append(state['image'])
+                    action = self.env.action_space.sample()
+                    next_obs, _, terminated, truncated, _ = self.env.step(action)
+                    if terminated or truncated:
+                        state = self.reset_get_state()
+                    else:
+                        state = self.obs2state(next_obs)
+            else:
+                with tqdm(total=self.cfg.data_size) as pbar:
+                    while len(imgs) < self.cfg.data_size:
+                        action = self.env.action_space.sample()
+                        next_obs, _, terminated, truncated, _ = self.env.step(action)
+                        if terminated or truncated:
+                            state = self.reset_get_state()
+                        else:
+                            state = self.obs2state(next_obs)
+                            imgs.append(state['image'])
+                            pbar.update(1)
+            self.imgs = np.array(imgs)
+            np.save(data_path, self.imgs)
+        else:
+            self.imgs = np.load(data_path)
+
+    def train(self):
+        train_imgs, test_imgs = th.utils.data.random_split(self.imgs, [0.7, 0.3])
+        train_data = th.utils.data.DataLoader(dataset=train_imgs, batch_size=self.cfg.batch_size, shuffle=True)
+        test_list = list(test_imgs)
+
+        for epoch in tqdm(range(self.cfg.nepochs)):
+            train_loss = []
+            self.cfg.fe.model.train()
+            for x in train_data:
+                x = x.to(self.cfg.device)
+                loss = self.cfg.fe.model.loss(x)
+                self.cfg.fe.model.optim.zero_grad()
+                loss.backward()
+                self.cfg.fe.model.optim.step()
+                train_loss.append(loss.cpu().detach().numpy())
+            train_loss = np.mean(train_loss)
+
+            self.cfg.fe.model.eval()
+            with th.no_grad():
+                x = test_imgs.to(self.cfg.device)
+                test_loss = self.cfg.fe.model.loss(x)
+                test_loss = np.mean(test_loss.cpu().detach().numpy())
+            self.writer.add_scalar('train/loss', train_loss, epoch+1)
+            self.writer.add_scalar('test/loss', test_loss, epoch+1)
+            if check_freq(self.cfg.nepochs, epoch, self.cfg.save_recimg_num):
+                x = th.tensor(random.choice(test_list)).to(self.cfg.device)
+                _, y = self.cfg.fe.model.forward(x, return_pred=True)
+                x = x.squeeze()
+                y = y.squeeze()
+                self.writer.add_image('rgb/'+str(epoch+1)+'_original', x[:3], epoch+1)
+                self.writer.add_image('depth/'+str(epoch+1)+'_original', x[3:], epoch+1)
+                self.writer.add_image('rgb/'+str(epoch+1)+'_reconstructed', y[:3], epoch+1)
+                self.writer.add_image('depth/'+str(epoch+1)+'_reconstructed', y[3:], epoch+1)
+            if self.es(test_loss, self.cfg.fe.model):
+                break
+    
+    def __call__(self):
+        self.gathering_data()
+        self.train()
+        self.close()
+
+class CombExecuter(Executer):
+    def __init__(self, env_name: str, cfg: CombConfig, options=None):
+        super().__init__()
+        gym_cable.register_robotics_envs()
+        self.env = gym.make(env_name, render_mode="rgb_array", max_episode_steps=cfg.nsteps, is_random=cfg.is_random)
+        self.writer = SummaryWriter(log_dir=cfg.output_dir)
+        self.act_space = gym.spaces.Box(
+            low = self.env.action_space.low[:cfg.rl.act_dim],
+            high = self.env.action_space.high[:cfg.rl.act_dim]
+        )
+        self.cfg = cfg
+        self.options = options
+        self.update_count = 1
+        
+        self.cfg.fe.model.load_state_dict(th.load(f'./model/{self.cfg.fe.model_name}', map_location=self.cfg.device))
+        self.cfg.fe.model.eval()
+        self.cfg.rl.model.train()
+    
+    def obs2state(self, obs, image_list=['rgb_image', 'depth_image']):
+        normalized_obs = self.normalize_state(obs)
+        image = normalized_obs[image_list[0]]
+        for name in image_list[1:]:
+            image = np.concatenate([image, normalized_obs[name]], axis=2)
+        image = th.tensor(self.cfg.fe.trans(image), dtype=th.float, device=self.cfg.device)
+        hs = self.cfg.fe.model.forward(image).cpu().squeeze().detach().numpy()
+        state = np.concatenate([hs, normalized_obs['observation'][:self.cfg.rl.obs_dim]])
         return state
         
     def set_action(self, state: np.ndarray, action: np.ndarray):
@@ -165,26 +274,22 @@ class CombExecuter:
     def train(self):
         frames = []
         titles = []
+        anim_path = os.path.join(self.cfg.output_dir, f'{self.cfg.basename}-1.mp4')
         try:
             self.train_epsiode_loop(frames=frames, titles=titles)
         except KeyboardInterrupt:
             self.cfg.rl.model.save(os.path.join(self.cfg.output_dir, f'{self.cfg.basename}.pth'))
-            anim(frames, titles=titles, filename=f'{self.cfg.output_dir}/{self.cfg.basename}-1.mp4', show=False)
+            anim(frames, titles=titles, filename=anim_path, show=False)
             sys.exit(1)
-        anim(frames, titles=titles, filename=f'{self.cfg.output_dir}/{self.cfg.basename}-1.mp4', show=False)
+        anim(frames, titles=titles, filename=anim_path, show=False)
     
     def test(self):
         frames = []
         titles= []
         self.test_step_loop(frames=frames, titles=titles)
-        anim(frames, titles=titles, filename=f'{self.cfg.output_dir}/{self.cfg.basename}-2.mp4', show=False)
+        anim(frames, titles=titles, filename=os.path.join(self.cfg.output_dir, f'{self.cfg.basename}-2.mp4'), show=False)
         self.cfg.rl.model.save(os.path.join(os.getcwd(), 'model', f'{self.cfg.basename}.pth'))
         self.cfg.rl.model.save(os.path.join(self.cfg.output_dir, f'{self.cfg.basename}.pth'))
-    
-    def close(self):
-        self.env.close()
-        self.writer.flush()
-        self.writer.close()
     
     def __call__(self):
         self.gathering_data()
@@ -222,7 +327,7 @@ class CombPPOExecuter(CombExecuter):
         episode_reward, episode_length = 0, 0
         state = self.reset_get_state()
         for step in tqdm(range(self.cfg.rl.model.buffer.memory_size), leave=False):
-            act, v, logp = self.cfg.rl.model.ac.step(torch.as_tensor(state, dtype=torch.float32, device=self.cfg.device))
+            act, v, logp = self.cfg.rl.model.ac.step(th.as_tensor(state, dtype=th.float32, device=self.cfg.device))
             transition = self.set_action(state, act)
             self.cfg.rl.model.buffer.append(transition['state'], transition['action'], transition['reward'], v, logp)
             episode_reward += transition['reward']
@@ -236,7 +341,7 @@ class CombPPOExecuter(CombExecuter):
                 self.writer.add_scalar('train/reward', episode_reward, self.ep)
                 self.writer.add_scalar('train/step', episode_length, self.ep)
                 if timeout or epoch_ended:
-                    _, v, _ = self.cfg.rl.model.ac.step(torch.as_tensor(state, dtype=torch.float32, device=self.cfg.device))
+                    _, v, _ = self.cfg.rl.model.ac.step(th.as_tensor(state, dtype=th.float32, device=self.cfg.device))
                 else:
                     v = 0
                 self.cfg.rl.model.buffer.finish_path(v)
---
+++ train_fe.py
@@ -0,0 +1,23 @@
+import hydra
+from omegaconf import OmegaConf
+
+from config import TrainFEConfig
+from executer import FEExecuter
+
+from utils import yes_no_input
+
+@hydra.main(config_path='conf/train_fe', config_name='train_fe', version_base=None)
+def main(_cfg: OmegaConf):
+    cfg = TrainFEConfig.convert(_cfg)
+    print(f'\n{cfg}\n')
+
+    # if not yes_no_input("fe.model_name, basename and data_name"):
+    #     exit()
+    
+    executer = FEExecuter(env_name='MZ04CableGrasp-v0', cfg=cfg)
+    del cfg, _cfg
+
+    executer()
+
+if __name__ == '__main__':
+    main()
