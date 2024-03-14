c3a1c2daae8bf5b478fc4443c2e0fb0045c17277
yuki_y <chrisrerei125815@gmail.com>
2024-03-14 20:49:29 +0900

    報酬の変更とその他

--- rl_w-ae.py
+++ rl_w-ae.py
@@ -2,18 +2,18 @@ import hydra
 from omegaconf import OmegaConf
 
 from utils import yes_no_input
-from config import PPOConfig
-from executer import CombPPOExecuter
+from config import CombConfig
+from executer import CombExecuter
 
-@hydra.main(config_path='conf/', config_name='ppo', version_base=None)
+@hydra.main(config_path='conf/', config_name='config', version_base=None)
 def main(_cfg: OmegaConf):
-    cfg = PPOConfig.convert(_cfg)
+    cfg = CombConfig.convert(_cfg)
     print(f'\n{cfg}\n')
     
     if not yes_no_input('fe.model_name and basename'):
         exit()
     
-    executer = CombPPOExecuter(env_name='MZ04CableGrasp-v0', cfg=cfg)
+    executer = CombExecuter(env_name='MZ04CableGrasp-v0', cfg=cfg)
     del cfg, _cfg
 
     executer()
