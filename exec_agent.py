ad132b844d861f12f7e34d86d9b01a9720210c0b
yuki_y <chrisrerei125815@gmail.com>
2024-03-28 15:47:58 +0900

    複数回の実行に変更

--- exec_agent.py
+++ exec_agent.py
@@ -12,11 +12,12 @@ def main(_cfg: OmegaConf):
     print(f'\n{cfg}\n')
     
     executer = CombExecuter(env_name='MZ04CableGrasp-v0', cfg=cfg)
-    executer.cfg.rl.model.load_state_dict(torch.load('logs/SAC_trainedDCAE_o-3_a-3/20240226-2249/SAC_trainedDCAE_o-3_a-3.pth', map_location=executer.cfg.device))
+    executer.cfg.rl.model.load_state_dict(torch.load('logs/SAC_trainedDCAE_o-6_a-6/20240318-1237/SAC_trainedDCAE_o-6_a-6_r.pth', map_location=executer.cfg.device))
     
     frames = []
     titles= []
-    executer.test_step_loop(frames=frames, titles=titles)
+    for _ in range(10):
+        executer.test_step_loop(frames=frames, titles=titles)
     anim(frames, titles=titles, filename=f'{executer.cfg.output_dir}/{executer.cfg.basename}-2.mp4', show=False)
     
     executer.close()
