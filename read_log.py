2125be6888df993d268466e16a9b1cfe396b6119
maki8maki <maki8maki.tech@gmail.com>
2024-04-25 15:10:02 +0900

    ログから画像を取り出すものを実装

--- read_log.py
+++ read_log.py
@@ -1,11 +1,13 @@
+import os
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow as tf
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
 
-def plot(log_dir, tagname=None, title=None, xlabel=None, ylabel=None):
+def plot(log_dir, tagnames=None, title=None, xlabel=None, ylabel=None):
     log_files = [path for path in Path(log_dir).glob("**/*") if path.is_file()]
 
     for log_file in log_files:
@@ -15,7 +17,7 @@ def plot(log_dir, tagname=None, title=None, xlabel=None, ylabel=None):
         tags = event.Tags()["scalars"]
 
         for tag in tags:
-            if tagname is not None and tag != tagname:
+            if tagnames is not None and tag in tagnames:
                 continue
             scalars = event.Scalars(tag)
             step = []
@@ -40,7 +42,31 @@ def plot(log_dir, tagname=None, title=None, xlabel=None, ylabel=None):
             plt.show()
 
 
+def show_image(log_dir, tagnames=None, is_save=False):
+    log_files = [path for path in Path(log_dir).glob("**/*") if path.is_file()]
+
+    for log_file in log_files:
+        event = EventAccumulator(str(log_file))
+        event.Reload()
+
+        tags = event.Tags()["images"]
+
+        for tag in tags:
+            if tagnames is not None and tag not in tagnames:
+                continue
+            imgs = event.Images(tag)
+            img = tf.image.decode_image(imgs[-1].encoded_image_string).numpy()
+            print(tag)
+            plt.imshow(img)
+            plt.axis("off")
+
+            if is_save:
+                plt.savefig(os.path.join("logs", tag.replace("/", "_") + ".pdf"))
+
+            plt.show()
+
+
 if __name__ == "__main__":
-    log_dir = "logs/SAC_w-TrainedDCAE/20231227-1913_xyz"
-    tagname = "test/reward"
-    plot(log_dir, tagname, title="", xlabel="episode", ylabel="rewards")
+    log_dir = "logs/DCAE/20240424-1908"
+    tagnames = ["depth/200_reconstructed", "rgb/200_reconstructed", "depth/200_original", "rgb/200_original"]
+    show_image(log_dir, tagnames, is_save=True)
