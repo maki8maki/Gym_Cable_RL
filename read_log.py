23c69a43a75de9c66f8c680db20f37827e3767d7
maki8maki <maki8maki.tech@gmail.com>
2024-06-25 11:58:14 +0900

    log読み取り関数を変更

--- read_log.py
+++ read_log.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
 
-def plot(log_dir, tagnames=None, title=None, xlabel=None, ylabel=None):
+def plot(log_dir, tagnames=None, title=None, xlabel=None, ylabel=None, filename=None):
     log_files = [path for path in Path(log_dir).glob("**/*") if path.is_file()]
 
     for log_file in log_files:
@@ -17,7 +17,7 @@ def plot(log_dir, tagnames=None, title=None, xlabel=None, ylabel=None):
         tags = event.Tags()["scalars"]
 
         for tag in tags:
-            if tagnames is not None and tag in tagnames:
+            if tagnames is not None and tag not in tagnames:
                 continue
             scalars = event.Scalars(tag)
             step = []
@@ -32,14 +32,15 @@ def plot(log_dir, tagnames=None, title=None, xlabel=None, ylabel=None):
             plt.tick_params(labelsize=15)
             if title is not None:
                 plt.title(title)
-            else:
-                plt.title(tag)
             if xlabel is not None:
                 plt.xlabel(xlabel, fontsize=18)
             if ylabel is not None:
                 plt.ylabel(ylabel, fontsize=18)
             plt.tight_layout()
-            plt.show()
+            if filename is not None:
+                plt.savefig(filename)
+            else:
+                plt.show()
 
 
 def show_image(log_dir, tagnames=None, is_save=False):
@@ -56,9 +57,10 @@ def show_image(log_dir, tagnames=None, is_save=False):
                 continue
             imgs = event.Images(tag)
             img = tf.image.decode_image(imgs[-1].encoded_image_string).numpy()
-            print(tag)
+            fig, _ = plt.subplots(figsize=(img.shape[1] / 10, img.shape[0] / 10))
             plt.imshow(img)
             plt.axis("off")
+            fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 
             if is_save:
                 plt.savefig(os.path.join("logs", tag.replace("/", "_") + ".pdf"))
