89beb8520dc0ff3a44c3fc1a829cf6325f886e8f
maki8maki <maki8maki.tech@gmail.com>
2024-06-25 11:56:56 +0900

    anim関数でintervalを変更できるようにした

--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ def set_seed(seed):
         th.cuda.manual_seed_all(seed)
 
 
-def anim(frames, titles=None, filename=None, show=True):
+def anim(frames, titles=None, filename=None, show=True, interval=50):
     plt.figure(figsize=(frames[0].shape[1] / 72.0, frames[0].shape[0] / 72.0), dpi=144)
     patch = plt.imshow(frames[0])
     plt.axis("off")
@@ -28,7 +28,7 @@ def anim(frames, titles=None, filename=None, show=True):
         if titles is not None:
             plt.title(titles[i], fontsize=32)
 
-    anim = animation.FuncAnimation(plt.gcf(), animate, frames=len(frames), interval=50)
+    anim = animation.FuncAnimation(plt.gcf(), animate, frames=len(frames), interval=interval)
     if filename is not None:
         anim.save(filename, writer="ffmpeg")
     if show:
