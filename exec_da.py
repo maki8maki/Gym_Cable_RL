7f348f28842f84849e44db899abbe1fc2f290783
maki8maki <maki8maki.tech@gmail.com>
2024-12-04 18:03:15 +0900

    適切な画像が得られるように変更

--- exec_da.py
+++ exec_da.py
@@ -15,7 +15,7 @@ def main(_cfg: OmegaConf):
     cfg = DAConfig.convert(_cfg)
 
     model = cfg.model
-    model.load(os.path.join("logs", "FE_CycleGAN_VAE", "20241015-1601", "FE_CycleGAN_VAE.pth"))
+    model.load(os.path.join("logs", "FE_CycleGAN_VAE", "20241023-1422", "FE_CycleGAN_VAE.pth"))
     model.eval()
 
     sim = np.load("data/sim.npy").astype(np.float32)
@@ -29,38 +29,54 @@ def main(_cfg: OmegaConf):
     fake_real = model.fake_B.cpu().squeeze().numpy().transpose(1, 2, 0) * 0.5 + 0.5
     fake_sim = model.fake_A.cpu().squeeze().numpy().transpose(1, 2, 0) * 0.5 + 0.5
 
-    sim = sim.squeeze().transpose(1, 2, 0)
-    real = real.squeeze().transpose(1, 2, 0)
+    sim = sim.squeeze().transpose(1, 2, 0) * 0.5 + 0.5
+    real = real.squeeze().transpose(1, 2, 0) * 0.5 + 0.5
 
+    fig, _ = plt.subplots(figsize=(sim.shape[1] / 10, sim.shape[0] / 10))
     plt.axis("off")
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
     plt.imshow(sim[..., :3], vmin=0, vmax=1)
     plt.show()
 
+    fig, _ = plt.subplots(figsize=(sim.shape[1] / 10, sim.shape[0] / 10))
     plt.axis("off")
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
     plt.imshow(sim[..., 3:], cmap="gray", vmin=0, vmax=1)
     plt.show()
 
+    fig, _ = plt.subplots(figsize=(sim.shape[1] / 10, sim.shape[0] / 10))
     plt.axis("off")
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
     plt.imshow(fake_real[..., :3], vmin=0, vmax=1)
     plt.show()
 
+    fig, _ = plt.subplots(figsize=(sim.shape[1] / 10, sim.shape[0] / 10))
     plt.axis("off")
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
     plt.imshow(fake_real[..., 3:], cmap="gray", vmin=0, vmax=1)
     plt.show()
 
+    fig, _ = plt.subplots(figsize=(sim.shape[1] / 10, sim.shape[0] / 10))
     plt.axis("off")
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
     plt.imshow(real[..., :3], vmin=0, vmax=1)
     plt.show()
 
+    fig, _ = plt.subplots(figsize=(sim.shape[1] / 10, sim.shape[0] / 10))
     plt.axis("off")
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
     plt.imshow(real[..., 3:], cmap="gray", vmin=0, vmax=1)
     plt.show()
 
+    fig, _ = plt.subplots(figsize=(sim.shape[1] / 10, sim.shape[0] / 10))
     plt.axis("off")
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
     plt.imshow(fake_sim[..., :3], vmin=0, vmax=1)
     plt.show()
 
+    fig, _ = plt.subplots(figsize=(sim.shape[1] / 10, sim.shape[0] / 10))
     plt.axis("off")
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
     plt.imshow(fake_sim[..., 3:], cmap="gray", vmin=0, vmax=1)
     plt.show()
 
